d0c453d4f0a9511ed79721d8dd212effd1d9a3de
Programezy <syavawow97@gmail.com>
2016-12-18 00:52:38 +0300

    Model PlayList + Track

---
+++ MultiThreadMusicPlayer/Media/PlayList.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadMusicPlayer.Media
+{
+    public class PlayList : IEnumerable
+    {
+        private Track[] _Tracks = new Track[0];
+
+        private string _ID;
+        public string ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+        private string _Name;
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        private TimeSpan _Length;
+        public TimeSpan Length
+        {
+            get { return _Length; }
+            set { _Length = value; }
+        }
+
+        private double _Rating;
+
+        public double Rating
+        {
+            get { return _Rating; }
+        }
+
+        private int _Count;
+
+        public int Count
+        {
+            get { return _Count; }
+        }
+
+        public PlayList()
+        {
+
+        }
+
+        public PlayList(params Track[] tracks)
+        {
+            foreach (var track in tracks)
+                Add(track);
+        }
+
+        public void Add(Track track)
+        {
+            if (_Count == _Tracks.Length)
+                Array.Resize(ref _Tracks, _Tracks.Length + 8);
+
+            _Tracks[_Count] = track;
+            _Count++;
+            RefreshRating();
+
+        }
+
+        public bool Remove(Track track)
+        {
+            for(int i = 0; i < _Count; i++)
+            {
+                if(_Tracks[i].Equals(track))
+                {
+                    for (int j = i; j < _Count; j++)
+                        _Tracks[j] = _Tracks[j + 1];
+                    _Count--;
+                    RefreshRating();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void RefreshRating()
+        {
+            double rate = 0 ;
+
+            foreach(Track track in this)
+            {
+                rate += track.Rating;
+            }
+
+            _Rating = rate / _Count;
+        }
+
+        private void RefreshLength()
+        {
+            TimeSpan length = new TimeSpan();
+
+            foreach(Track track in this)
+            {
+                length += track.Length;
+            }
+
+            Length = length;
+
+        }
+
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < _Tracks.Length; i++)
+                yield return _Tracks[i];      
+        }
+    }
+}
---
+++ MultiThreadMusicPlayer/Media/Track.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadMusicPlayer.Media
+{
+    public class Track
+    {
+        public enum Genres { Rock, Rap, Classic, Electro};
+
+        private string _ID;
+
+        public string ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+
+        }
+
+        private string _Name;
+
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value; }
+        }
+
+        private TimeSpan _Length;
+        public TimeSpan Length
+        {
+            get { return _Length; }
+            set { _Length = value; }
+        }
+
+        private string _Author;
+
+        public string Author
+        {
+            get { return _Author; }
+            set { _Author = value; }
+        }
+
+        private Genres _Genre;
+
+        public Genres Genre
+        {
+            get { return _Genre; }
+            set { _Genre = value; }
+        }
+
+        private int _Rating;
+
+        public int Rating
+        {
+            get
+            {
+                return _Rating;
+            }
+
+            set
+            {
+                if (value > 10 || value < 0)
+                {
+                    throw new IndexOutOfRangeException("Rating is out of range");
+                }
+
+                _Rating = value;
+            }
+        }
+    }
+}
