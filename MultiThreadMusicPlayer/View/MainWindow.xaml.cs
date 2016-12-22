397e8290ce15b7a946c97f8422d34d75a1aab6e0
Tim <Tim@TIM-PC>
2016-12-22 20:58:10 +0300

    Thread Complete

--- MultiThreadMusicPlayer/View/MainWindow.xaml.cs
+++ MultiThreadMusicPlayer/View/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,101 @@ namespace MultiThreadMusicPlayer
     /// </summary>
     public partial class MainWindow : Window
     {
+      
+        MediaPlayer MPlayerOne;
+        MediaPlayer MPlayerTwo;
+        private bool IsOpened;
+
         public MainWindow()
         {
             InitializeComponent();
+            MPlayerOne = new MediaPlayer();
+            MPlayerTwo = new MediaPlayer();
         }
+
+        private void Play(object player)
+        {
+            MediaPlayer Player = player as MediaPlayer;
+
+            Player.Play();
+        }
+
+        private void Pause(object player)
+        {
+            var Player = player as MediaPlayer;
+            Player.Pause();
+
+        }
+
+        private void Stop(object player)
+        {
+            var Player = player as MediaPlayer;
+            Player.Stop();
+        }
+
+        private void Open_Track_1(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.DefaultExt = ".mp3";
+            fd.Filter = "Music files (.mp3)|*.mp3";
+            bool? result = fd.ShowDialog();
+
+            if (result == true)
+            {
+                MPlayerOne.Open(new Uri(fd.FileName, UriKind.Absolute));
+                IsOpened = true;
+            }
+        }
+
+        private void Play_1(object sender, RoutedEventArgs e)
+        {
+            if (IsOpened)
+                Dispatcher.BeginInvoke(new ParameterizedThreadStart(Play), MPlayerOne);
+
+        }
+
+        private void Pause_1(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new ParameterizedThreadStart(Pause), MPlayerOne);
+        }
+
+        private void Stop_1(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new ParameterizedThreadStart(Stop), MPlayerOne);
+        }
+
+        private void Open_Track_2(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.DefaultExt = ".mp3";
+            fd.Filter = "Music files (.mp3)|*.mp3";
+            bool? result = fd.ShowDialog();
+
+            if (result == true)
+            {
+                MPlayerTwo.Open(new Uri(fd.FileName, UriKind.Absolute));
+                IsOpened = true;
+            }
+        }
+
+        private void Play_2(object sender, RoutedEventArgs e)
+        {
+            if (IsOpened)
+                Dispatcher.BeginInvoke(new ParameterizedThreadStart(Play), MPlayerTwo);
+
+        }
+
+        private void Pause_2(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new ParameterizedThreadStart(Pause), MPlayerTwo);
+        }
+
+        private void Stop_2(object sender, RoutedEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new ParameterizedThreadStart(Stop), MPlayerTwo);
+        }
+
+
     }
 }
+
